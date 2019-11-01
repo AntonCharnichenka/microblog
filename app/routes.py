d1903445ad3a57880967fe0c166dcba0ba85b648
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2019-11-01 08:50:54 +0300

    Part2:Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'username': 'Anton'}
+    posts = [
+        {
+            'author': {'username': 'Tania'},
+            'body': 'I like gelatto!'
+        },
+        {
+            'author': {'username': 'Masha'},
+            'body': 'I like playing dols.'
+        },
+        {
+            'author': {'username': 'Vitia'},
+            'body': 'I like tools.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

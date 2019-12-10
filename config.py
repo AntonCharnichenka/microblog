0e6863da0a758784cc186623221c01955eb37db9
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2019-12-10 09:20:44 +0300

    Part7:error handling

--- config.py
+++ config.py
@@ -6,5 +6,13 @@ DEFAULT_VALUE = 'lolkek'
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or DEFAULT_VALUE
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///{}'.format(os.path.join(BASEDIR, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATION = False
+
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or '25')
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['waitforyourletter@gmail.com']

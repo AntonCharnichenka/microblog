97014323a8217c1c34d542aa9e805b44416d9d8c
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2019-12-15 16:29:54 +0300

    Part10: email support

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 from config import Config
 
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 db_migration_engine = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+mail = Mail(app)
 
 # Sending Errors by Email and Logging to a File
 if not app.debug:

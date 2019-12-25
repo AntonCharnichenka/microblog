05e58f3ea70b123bd2b353016aa8c0ab4b922fe7
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2019-12-25 13:48:50 +0300

    App factory function

--- microblog.py
+++ microblog.py
@@ -1,10 +1,9 @@
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port='5000')
+app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post' :Post}
